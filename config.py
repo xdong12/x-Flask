08f9aca445ee516ebeeb919baa6bfb8dd7d5999d
xdong12 <xdong12@163.com>
2019-02-27 18:50:31 +0800

    数据库集成

--- config.py
+++ config.py
@@ -4,7 +4,24 @@
 # @Time: 19-2-24 上午11:11
 # @File: config.py
 
+
+import redis
+
+
 class Config(object):
     """对项目进行配置，一些配置项写在这里"""
     DEBUG = True
     SECRET_KEY = "!@#$%^"
+    # mysql数据库链接地址
+    SQLALCHEMY_DATABASE_URI = "mysql://root:mysql@127.0.0.1:3306/xflask"
+    # 设置不跟踪数据库的修改
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    # redis数据库相关配置
+    REDIS_HOST = "127.0.0.1"
+    REDIS_PORT = 6379
+    SESSION_TYPE = "redis"  # 存储类型
+    SESSION_KEY_PREFIX = "Session:"
+    SESSION_USE_SIGNER = True  # 签名存储
+    SESSION_REDIS = redis.StrictRedis(host=REDIS_HOST,
+                                      port=REDIS_PORT, db=10)  # 制定存储位置
+    PERMANENT_SESSION_LIFETIME = 3600 * 24 * 2  # 设置session两天有效
--- hello.py
+++ hello.py
@@ -6,28 +6,66 @@
 
 
 from datetime import datetime
-from flask import Flask, render_template, session, url_for
-from flask import flash
+from flask import Flask, render_template, session, url_for, flash, redirect
 from flask import make_response
-from flask import redirect
 from flask import request
-from flask_script import Manager
+from flask_sqlalchemy import SQLAlchemy
+from flask_session import Session
+from flask_script import Manager, Shell
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
 from config import Config
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from flask_wtf import Form
+from flask_migrate import Migrate, MigrateCommand
 
+
+
+
+
+#主页表单
 class NameForm(Form):
     name = StringField("What is your name?", validators=[DataRequired()])
     submit = SubmitField("Submit")
+
 
 app = Flask(__name__)  # 初始化
 app.config.from_object(Config)
 manager = Manager(app)
 bootstrap = Bootstrap(app)
 moment = Moment(app)
+redis_store = Config.SESSION_REDIS
+Session(app)
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+manager.add_command('db', MigrateCommand)
+
+class Role(db.Model):
+    __tablename__ = 'roles'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64), unique=True)
+
+    users = db.relationship('User', backref='role')
+
+    def __repr__(self):
+        return '<Role %r>' % self.name
+
+
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), unique=True, index=True)
+
+    role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
+
+    def __repr__(self):
+        return '<User %r>' % self.username
+
+
+def make_shell_context():
+    return dict(app=app, db=db, User=User, Role=Role)
+manager.add_command("shell", Shell(make_context=make_shell_context))
 
 
 @app.route('/', methods=['GET', 'POST'])  # 接收POST请求
@@ -40,14 +78,19 @@ def index():
         # name = form.name.data  #
         # form.name.data = ''  #把字段数据重设为空字符串,请空字符串
 
-        old_name = session.get('name')
-        if old_name and old_name != form.name.data:  # flash信息
-            flash("Looks like you have changed your name!")
+        user = User.query.filter(User.username==form.name.data).first()
+        if not user:
+            user = User(username=form.name.data)
+            db.session.add(user)
+            session['known'] = False
+        else:
+            session['known'] = True
 
-        session['name'] = form.name.data  # 获取name， 存到session
+        session['name'] = form.name.data
+        form.name.data = ''
+
         return redirect(url_for('index'))  # 重定向， 实现刷新时不重复提交的效果
-    return render_template('index.html', form=form, name=session.get('name'),
-                           current_time=datetime.utcnow())
+    return render_template('index.html', form=form, name=session.get('name'), known=session.get('known', False), current_time=datetime.utcnow())
 
 
 @app.route('/user/<name>')
@@ -65,6 +108,8 @@ def page_not_found(e):
 @app.errorhandler(500)
 def internal_server_error(e):
     return render_template('500.html'), 500
+
+
 
 if __name__ == '__main__':
     # app.run(debug=None, port=8888)  # 调试模式， 端口8888
